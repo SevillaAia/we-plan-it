d22b34f24ae38f7abb98cdfd662cb3e57249c153
Guia <guia.sevilla.gs@gmail.com>
2026-02-09 11:11:55 +0100

    we plan it

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header.tsx'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Header />
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+        <h1>We Plan It!</h1>
+        <h2>
+            Easy to use daily planner.
+        </h2>
+    </header>
+  )
+}
+
+export default Header
